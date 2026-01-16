1ce1c4ef5f4e34063fc4ac18326bf75aae70d3ec
gyubly <eoghkwo800@gmail.com>
2026-01-16 10:06:29 +0900

    html/css 학습내용 한꺼번에 업로드

---
+++ html:css study/q/debugtest.js
@@ -0,0 +1,19 @@
+let a;
+let b = [1, 2, 3, 4];
+let strings = ["아이템", "수치", "능력"];
+a = b[1];
+document.write(a);
+document.write("\n\n");
+
+a = a + 1;
+a = a + 1;
+a = b[3];
+let c = strings[2];
+document.write(a);
+document.write("\n\n");
+alert(a);
+alert(c);
+
+a = 0;
+a = a + 1;
+document.write(a);
---
+++ html:css study/q/index.js
@@ -0,0 +1,4 @@
+let a = "hello";
+let b = "world";
+
+alert(a + b);
